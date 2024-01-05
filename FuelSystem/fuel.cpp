471ea9c49cd16cd477ebdd0330ac0cc9fb71d3b3
Ryan Baker <mnbakerboy1@gmail.com>
2024-01-05 15:29:58 -0500

    Add creation of tanks.

--- FuelSystem/fuel.cpp
+++ FuelSystem/fuel.cpp
@@ -1,25 +1,80 @@
 #include "fuel.h"
 
 FuelSys::FuelSys() {
-
+	m_current = nullptr;
 }
 
 FuelSys::~FuelSys() {
+	Tank* nextTank = nullptr;
 
+	while (m_current != nullptr) {
+		nextTank = m_current->m_next;
+		delete m_current;
+		m_current = nextTank;
+	}
 }
 
+/*
 const FuelSys& FuelSys::operator=(const FuelSys& rhs) {
 
-}
+}*/
 
+/* Function: addTank
+ * -----------------
+ * tankID: unique ID
+ * capacity: How much fuel the tank can hold
+ * tankFuel: Current amount of fuel in the tank at add
+ *
+ * Tries to add a new tank to the list
+ *
+ * return: true if a tank was successfully created else false
+ */
 bool FuelSys::addTank(int tankID, int capacity, int tankFuel) {
+	if (tankFuel < 0 || tankID < 0 || capacity < 0) {
+		return false;
+	}
 
+	if (m_current == nullptr) {
+		m_current = new Tank(tankID, capacity, tankFuel);
+		return true;
+	}
+
+	Tank* lastTank = findEligibleTank(tankID);
+	if (lastTank == nullptr) {
+		return false;
+	}
+
+	lastTank->m_next = new Tank(tankID, capacity, tankFuel);
+
+	return true;
 }
 
+/* Function: findEligibleTank
+ * --------------------------
+ * tankID: potential new tank ID
+ *
+ * Checks the list of existing tanks for duplicate ID
+ *
+ * return: last tank in the list if no duplicate ID else nullptr
+ */
 Tank* FuelSys::findEligibleTank(int tankID) {
+	Tank* currentTank = m_current;
 
+	while (currentTank->m_next != nullptr) {
+		if (currentTank->m_tankID == tankID) {
+			return nullptr;
+		}
+		currentTank = currentTank->m_next;
+	}
+
+	if (currentTank->m_tankID == tankID) {
+		return nullptr;
+	}
+
+	return currentTank;
 }
 
+/*
 bool FuelSys::removeTank(int tankID) {
 
 }
@@ -54,3 +109,4 @@ void FuelSys::dumpSys() const {
 
 void FuelSys::dumpPumps(Pump* pumps) const {
 }
+*/
